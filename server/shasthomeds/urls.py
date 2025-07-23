fb600933fc83136b1bd55f6fa6096c3a24301da8
Farhana Bente Islam <21303195@iubat.edu>
2025-07-24 03:09:07 +0600

    update server urls.py

--- server/shasthomeds/urls.py
+++ server/shasthomeds/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('ShasthoMeds/user/', include('users.urls')),  # this line includes user-related routes like register/
 ]
