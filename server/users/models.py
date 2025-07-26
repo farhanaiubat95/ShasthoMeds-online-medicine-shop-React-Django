7321d7badf89fadec7328e014c3a767c7b252f48
Farhana Bente Islam <21303195@iubat.edu>
2025-07-26 20:38:31 +0600

    changes some parts of models.py, then migrate

---
+++ server/users/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+from django.conf import settings
+
+# Custom user model
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length=150, default="Your Name")
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, default="")
+    gender = models.CharField(max_length=10, default="Other")
+    city = models.CharField(max_length=100, default="Dhaka")
+    date_of_birth = models.DateField(default="2000-01-01")
+    address = models.TextField(default="")
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  
+
+# OTP model
+class EmailOTP(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    otp_code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=5)
+
+    def __str__(self):
+        return f"{self.user.email} - OTP: {self.otp_code}"
