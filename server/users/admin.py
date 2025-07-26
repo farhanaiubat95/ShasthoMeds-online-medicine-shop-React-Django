45c79e306f84564e507259d78732a002db1e9bdb
Farhana Bente Islam <21303195@iubat.edu>
2025-07-26 20:39:47 +0600

    Update admin.py

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, EmailOTP
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    
+
+    list_display = (
+        'full_name', 'username','email', 'phone', 'gender', 
+        'city', 'address', 'date_of_birth', 'is_verified'
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Personal Info", {
+            'fields': (
+                'full_name','phone', 'gender', 'date_of_birth',
+                'city', 'address', 'is_verified'  # ()'username', 'email') are already present in the default fieldsets
+            )
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(EmailOTP)
