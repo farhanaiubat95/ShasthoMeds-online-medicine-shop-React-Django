6bc08f46440de34251b6681bdb57756e8e85492f
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2025-08-02 02:07:49 +0600

    update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // Keep '/' if deploying to root domain
   plugins: [
     react(),
     tailwindcss(),
