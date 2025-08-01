7623fa46ea326490897ca67b06430dd43b715b29
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2025-08-02 01:09:50 +0600

    update client files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import Routers from './routers/Routers.jsx';
 // import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
 import Footer from './components/Footer.jsx';
+import ScrollToTop from './routers/ScrollToTop.jsx';
 
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <div className='mx-5 sm:mx-10 lg:mx-15 xl:mx-30'>
         <Header />
         <Routers />
