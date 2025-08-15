dae1b2e4be7b216d9628cd8bf55b6bc6faedfe1b
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2025-08-16 04:07:38 +0600

    Update al files

--- server/users/urls.py
+++ server/users/urls.py
@@ -4,8 +4,7 @@ from .views import (
     UpdateProfileView, VerifyOTPView, CustomTokenObtainPairView,
 
     # CRUD views
-    CategoryViewSet, BrandViewSet, ProductViewSet,
-    PrescriptionRequestViewSet, add_to_cart, get_or_create_cart,
+   
 )
 
 from rest_framework_simplejwt.views import ( # pyright: ignore[reportMissingImports]
@@ -14,12 +13,6 @@ from rest_framework_simplejwt.views import ( # pyright: ignore[reportMissingImpo
 )
 
 from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'category', CategoryViewSet, basename='category')
-router.register(r'brand', BrandViewSet, basename='brand')
-router.register(r'product', ProductViewSet, basename='product')
-router.register(r'prescription-request', PrescriptionRequestViewSet, basename='prescriptionrequest')
 
 
 urlpatterns = [
@@ -31,6 +24,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('update-profile/', UpdateProfileView.as_view(), name='update-profile'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
-    path('cart/', get_or_create_cart, name='cart-get-or-create'),
-    path('cart/add/', add_to_cart, name='cart-add'),
-] + router.urls
+    
+] 
