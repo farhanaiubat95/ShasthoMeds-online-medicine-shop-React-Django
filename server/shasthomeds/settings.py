07ed91d20c6fa6f266d1a1f09e9aff94c69dff3a
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2025-08-19 10:35:50 +0600

    Update files

--- server/shasthomeds/settings.py
+++ server/shasthomeds/settings.py
@@ -158,11 +158,16 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cloudinary (all media goes here)
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': config('CLOUDINARY_API_KEY'),
-    'API_SECRET': config('CLOUDINARY_API_SECRET'),
-}
+import cloudinary
+
+# Add this after your CLOUDINARY_STORAGE configuration
+cloudinary.config(
+    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
+    api_key=config('CLOUDINARY_API_KEY'),
+    api_secret=config('CLOUDINARY_API_SECRET'),
+    secure=True
+)
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # -------------------------
