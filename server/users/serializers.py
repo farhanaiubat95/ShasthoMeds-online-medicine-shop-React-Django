cbff9ede875e00998a08eb74f14abeafa91d3dc7
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2025-08-31 04:25:36 +0600

    Updates

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -140,7 +140,7 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name", "slug", "image", "parent", "is_active", "created_at", "updated_at"]
-        read_only_fields = ["id", "created_at", "updated_at", "slug"]
+        read_only_fields = ["id", "created_at", "updated_at"]
 
     def to_representation(self, instance):
         """Return Cloudinary URL for image instead of file path"""
