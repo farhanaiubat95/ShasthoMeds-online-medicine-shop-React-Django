d8684718ebd4cc1b24e62928b436acc17e8e077b
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2025-10-02 12:21:40 +0600

    Update

--- server/shasthomeds/settings.py
+++ server/shasthomeds/settings.py
@@ -152,12 +152,12 @@ EMAIL_USE_TLS=True
 EMAIL_HOST_USER="farhanasha0113@gmail.com"
 EMAIL_HOST_PASSWORD="nbmxanmmzkusbrii"
 
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_BACKEND = config('EMAIL_BACKEND')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # -------------------------
 # JWT Auth
